566080bfdf17e183c4ed90e2e0b459697bf1be31
kc <chandras@cs.purdue.edu>
2010-08-31 15:52:16 -0400

    threadlet.c backed up

--- multiMLton/trunk/runtime/gc/threadlet.c
+++ multiMLton/trunk/runtime/gc/threadlet.c
@@ -123,26 +123,38 @@ void GC_printStackTop (void) {
 }
 
 GC_thread GC_preemptAsync (GC_thread oriThrd, int startOffset) {
+    assert (isObjptr ((objptr)(oriThrd)));
     GC_state s = pthread_getspecific (gcstate_key);
 
     /* Find out the size needed for newThread */
-    GC_stack oriStk = (GC_stack) objptrToPointer (oriThrd->stack, s->heap->start);
-    int size = oriStk->used - startOffset;
+    long int size = -1;
+    {
+        GC_stack oriStk = (GC_stack) objptrToPointer (oriThrd->stack, s->heap->start);
+        size = oriStk->used - startOffset;
+    }
+    assert (size > 0);
 
     /* newThread () might do GC, so backup stuff */
     getStackCurrent(s)->used = sizeofGCStateCurrentStackUsed (s);
     getThreadCurrent(s)->exnStack = s->exnStack;
+    assert (s->savedThread == BOGUS_OBJPTR);
+    s->savedThread = pointerToObjptr ((pointer)oriThrd, s->heap->start);
 
     GC_thread th = newThread (s, size);
+    /* restore */
+    oriThrd = (GC_thread)(objptrToPointer(s->savedThread, s->heap->start));
+    s->savedThread = BOGUS_OBJPTR;
+
     GC_stack stk = (GC_stack) objptrToPointer (th->stack, s->heap->start);
 
     /* Find the limits of async */
-    oriStk = (GC_stack) objptrToPointer (oriThrd->stack, s->heap->start);
+    GC_stack oriStk = (GC_stack) objptrToPointer (oriThrd->stack, s->heap->start);
     pointer start = getStackBottom (s, oriStk) + startOffset;
     pointer end = getStackBottom (s, oriStk) + oriStk->used;
     assert (start < end);
 
     long int numBytes = end-start;
+    assert (size == numBytes);
     s->copiedSize = numBytes;
     if (DEBUG_SPLICE) {
         fprintf (stderr, "\tnumBytes = %ld\n", numBytes);
@@ -161,9 +173,9 @@ GC_thread GC_preemptAsync (GC_thread oriThrd, int startOffset) {
 }
 
 void GC_prefixAndSwitchTo (GC_state s, GC_thread thrd) {
-    /* TODO : Add checks to find out if we have space to copy the frames. If not,
-     * grow stack */
     /* TODO : get frameBottom by fixed offset */
+
+    assert (s->atomicState > 0);
 
     if (DEBUG_SPLICE)
         fprintf (stderr, "\nprefixAndSwitchTo [%d]\n", Proc_processorNumber (s));
@@ -172,10 +184,11 @@ void GC_prefixAndSwitchTo (GC_state s, GC_thread thrd) {
     pointer p = getStackBottom (s, stk);
     int size = stk->used;
 
+    assert (s->stackLimit > s->stackTop + size);
+
     pointer start = GC_getFrameBottom ();
     memcpy (start, p, size);
     s->stackTop = start + size;
-    assert (s->stackTop < s->stackLimit);
     if (DEBUG_SPLICE) {
         fprintf (stderr, "\tprefixing frame of size %d\n", size);
         fprintf (stderr, "\tnewStackTop = "FMTPTR"\n", (uintptr_t) s->stackTop);
