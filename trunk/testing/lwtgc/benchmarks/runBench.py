951b402d2df81837547b4d2b95d0ea7e5fca2d3d
kc <chandras@cs.purdue.edu>
2011-09-22 19:29:44 -0400

    findHeapRanges and runBench modified

--- trunk/testing/lwtgc/benchmarks/runBench.py
+++ trunk/testing/lwtgc/benchmarks/runBench.py
@@ -152,131 +152,79 @@ def testParameters():
 	numProcs = [16]
 	return (progName, args, numProcs)
 
-
-def getPointsInRange (min, max, numPoints):
-	result = []
-	step = (max - min)/(numPoints)
-	for i in range (0, numPoints):
-		result.append (round (min + i*step))
-	return result
-
-def getPointsRec (min, max, numPartitions, pointsPerPartition):
-	res = []
-	if (numPartitions):
-		cur = (min + max)/2
-		if (numPartitions == 1):
-			res += getPointsInRange (min, max, pointsPerPartition * 2)
-		else:
-			res += getPointsInRange (cur, max, pointsPerPartition)
-		res += getPointsRec (min, round (cur), numPartitions - 1, pointsPerPartition)
-	return res
-
-def getPoints (min, max):
-	#Total number of points = numPartitions * pointsPerPartition
-	numPartitions = 3
-	pointsPerPartition = 4
-	return getPointsRec (min, max, numPartitions, pointsPerPartition)
-
-def maxHeapLocalValues (b, n, progName, args, c):
-	atMLtons = ["number-processors " + str(n), \
-							"enable-timer 20000", \
-							"gc-summary individual"]
-	(t, m, ml, ms) = run ("./" + str(b), str(progName[b]), atMLtons, args[b])
-	maxHeapLocalMax = ml
-	if int(t) == 0:
-		sys.exit ("Initial run failed. Exiting....")
-
-	min = 0
-	max = maxHeapLocalMax
-	while (bytesIntToString (min, 0) != bytesIntToString (max, 0)):
-		print (bytesIntToString (min, 0))
-		print (bytesIntToString (max, 0))
-		cur = (min + max)/2
-		newAtMLtons = atMLtons + ["max-heap-local " + bytesIntToString (round(cur), 1)]
-		(t, m, ml, ms) = run ("./" + str(b), str(progName[b]), newAtMLtons, args[b])
-		if int(t) == 0:
-			print ("Repeat")
-			(t, m, ml, ms) = run ("./" + str(b), str(progName[b]), newAtMLtons, args[b])
-			if int(t) == 0:
-				min = cur
-			else:
-				max = cur
-		else:
-			max = cur
-	maxHeapLocalMin = cur
-
-	points = getPoints (maxHeapLocalMin, maxHeapLocalMax)
-	points.sort ()
-	print ("values for maxHeapLocal: " + str ([bytesIntToString (x, 1) for x in points]))
-	return points
-
-def maxHeapSharedValues (b, n, progName, args, c):
-	atMLtons = ["number-processors " + str(n), \
-							"enable-timer 20000", \
-							"gc-summary individual"]
-	(t, m, ml, ms) = run ("./" + str(b), str(progName[b]), atMLtons, args[b])
-	maxHeapSharedMax = ms
-	if int(t) == 0:
-		sys.exit ("Initial run failed. Exiting....")
-
-	min = 0
-	max = maxHeapSharedMax
-	while (bytesIntToString (min, 0) != bytesIntToString (max, 0)):
-		print (bytesIntToString (min, 0))
-		print (bytesIntToString (max, 0))
-		cur = (min + max)/2
-		newAtMLtons = atMLtons + ["max-heap-shared " + bytesIntToString (round(cur), 1)]
-		(t, m, ml, ms) = run ("./" + str(b), str(progName[b]), newAtMLtons, args[b])
-		if int(t) == 0:
-			min = cur
-		else:
-			max = cur
-	maxHeapSharedMin = cur
-
-	points = getPoints (maxHeapSharedMin, maxHeapSharedMax)
-	points.sort ()
-	print ("values for maxHeapShared: " + str ([bytesIntToString (x, 1) for x in points]))
-
-	return points
-
 def main():
-
+	reruns = 1
 	#Parse options
 	parser = OptionParser()
+	parser.add_option("-d", "--database", dest="database", help="database location", \
+										metavar="FILE", default="/home/chandras/PLDI/results")
 	parser.add_option("-b", "--benchmark", dest="bmarkList", action="append", \
 										help="run only the given benchmarks")
+	parser.add_option("-r", "--rerun", dest="rerun", default=False, \
+										help="Rerun a specific test even if the corresponding result is available in the database")
+
 	(options, args) = parser.parse_args()
+
+	#Connect to database
+	conn = sqlite3.connect(str(options.database))
+	c = conn.cursor ()
 
 	#benchmark parameters
 	if (options.bmarkList):
 		benchmarks = options.bmarkList
 	else:
 		benchmarks = ["BarnesHut2",	"CountGraphs"]
-
-	#Connect to database
-	conn = sqlite3.connect("/home/chandras/PLDI/results")
-	c = conn.cursor ()
-
-	c.execute ("create table if not exists heapRanges \
-							(benchmark text, numProcs int, gckind text, args text, maxSHV text, maxLHV text)")
-
 	(progName, args, numProcs) = fullParameters ()
+
+	#create the completed run if it is not already present
+	c.execute('create table if not exists completedRuns \
+						(benchmark text, numProcs int, maxHeapLocal text, maxHeapShared text, gckind text)')
+
+	#create the runtime table if it is not already present
+	c.execute('create table if not exists runTime \
+						(benchmark text, numProcs int, maxHeapLocal text, \
+						 maxHeapShared text, maxHeap text, gckind text, result int)')
 
 	for b in benchmarks:
 		for n in numProcs:
-			shouldRun = False
-			c.execute ("select * from heapRanges where benchmark=? and numProcs=? and args=? and gckind=?",
-								 (b, n, args[b], "WB"))
+			c.execute ("select maxSHV, maxLHV from heapRanges where benchmark=? and numProcs=? \
+									and gckind=? and args=?", (b, n, "WB", args[b]))
+			data = c.fetchall ()
+			if (not data):
+				print ("Benchmark: " + str(b) + " numProcs: " + str(n) + " -- heapRanges not found!!")
+				continue
+			sys.exit ("STOP!")
 
-			if (not c.fetchall ()):
-				shouldRun = True
+			for ms in maxHeapShared:
+				for ml in maxHeapLocal:
+					atMLtons = ["number-processors " + str(n), \
+											"max-heap-local " + str(ml), \
+											"max-heap-shared " + str(ms), \
+											"enable-timer 20000"]
 
-			if (shouldRun):
-				maxSHV = maxHeapSharedValues (b, n, progName, args, c)
-				maxLHV = maxHeapLocalValues (b, n, progName, args, c)
-				#insert into DB
-				c.execute ("insert into heapRanges values (?, ?, ?, ?, ?, ?)",
-										(b, n, "WB", args[b], str(maxSHV), str(maxLHV)))
-				conn.commit ()
+					#run only if required
+					shouldRun = True
+					if (options.rerun == False):
+						c.execute ("select * from completedRuns where benchmark=? and numProcs=? and maxHeapLocal=? \
+												and maxHeapShared=? and gckind=?", (b, n, ml, ms, "WB"))
+						data = c.fetchall ()
+						if (data):
+							shouldRun = False
+
+						if (shouldRun):
+							r, m, mlr, msr = 0, 0, 0, 0
+							for i in range(0, reruns):
+								(_r, _m, _mlr, _msr) = run ("./" + str(b), str(progName[b]), atMLtons, args[b])
+								r += int(_r)
+								m += int(_m)
+								mlr += int(_mlr)
+								msr += int(_msr)
+
+							r, m, mlr, msr = r/reruns, m/reruns, mlr/reruns, msr/reruns
+							c.execute ('insert into runTime values (?, ?, ?, ?, ?, ?, ?)', \
+												(b, n, convert_bytes (mlr), convert_bytes (msr), convert_bytes (m), "WB", int(r)))
+							c.execute ("insert into completedRuns values (?, ?, ?, ?, 'RB')",\
+												(b, n, ml, ms))
+							conn.commit ()
 
 main ()
