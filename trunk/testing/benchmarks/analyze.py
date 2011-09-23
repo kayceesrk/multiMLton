53c8b83f77d789a605009e8e71d296ccfbd6e156
kc <chandras@cs.purdue.edu>
2011-09-22 20:56:15 -0400

    Edits to analyze

--- trunk/testing/benchmarks/analyze.py
+++ trunk/testing/benchmarks/analyze.py
@@ -1,16 +1,93 @@
 import subprocess
 import shlex
 import re
-import sqlite3
 from optparse import OptionParser
 import numpy
 import matplotlib.pyplot as plt
+import sys
+import os
+import fnmatch
+import locale
+import sqlite3
 
 logging=True
+
+def bytesIntToString (bytes, decimal):
+	bytes = float(bytes)
+	if bytes >= 1099511627776:
+		terabytes = bytes / 1099511627776
+		size = str (round (terabytes, decimal)) + "T"
+	elif bytes >= 1073741824:
+		gigabytes = bytes / 1073741824
+		size = str (round (gigabytes, decimal)) + "G"
+	elif bytes >= 1048576:
+		megabytes = bytes / 1048576
+		size = str (round (megabytes, decimal)) + "M"
+	elif bytes >= 1024:
+		kilobytes = bytes / 1024
+		size = str (round (kilobytes, decimal)) + "K"
+	else:
+		size = str (round (bytes, decimal)) + "b"
+	return size
+
+def bytesIntToString2 (bytes, decimal):
+	bytes = float(bytes)
+	if bytes >= 1099511627776:
+		terabytes = bytes / 1099511627776
+		size = '%.1fT' % terabytes
+		size = str (round (terabytes, decimal)) + "T"
+	elif bytes >= 1073741824:
+		gigabytes = bytes / 1073741824
+		size = '%.1fG' % gigabytes
+	elif bytes >= 1048576:
+		megabytes = bytes / 1048576
+		size = '%.1fM' % megabytes
+	elif bytes >= 1024:
+		kilobytes = bytes / 1024
+		size = '%.1fK' % kilobytes
+	else:
+		size = '%.1fb' % bytes
+	return size
+
+def bytesStringToInt (s):
+	intVal = float(s.replace('K','').replace('M','').replace('G',''))
+	if s.endswith('K'):
+		intVal *= 1024
+	elif s.endswith('M'):
+		intVal *= 1024*1024
+	elif s.endswith('G'):
+		intVal *= 1024*1024*1024
+	return intVal
 
 def log(s):
     if (logging):
         print (s)
+
+def getMem(dir):
+	totalLocal = 0
+	shared = 0
+
+	for file in os.listdir (dir):
+		if fnmatch.fnmatch (file, 'gc-summary.*.out') and (not fnmatch.fnmatch (file, 'gc-summary.cumul.out')):
+			infile = open (dir + "/" + file, "r")
+			for line in infile.readlines ():
+				if line.startswith ("max local heap size"):
+					m = re.sub (r'max local heap size: (.*?) bytes', r'\1', line).rstrip ()
+					totalLocal += int (re.sub (r',', r'', m))
+
+	infile = open (dir + "/gc-summary.cumul.out", "r")
+	for line in infile.readlines ():
+		if line.startswith ("max shared heap size"):
+			m = re.sub (r'max shared heap size: (.*?) bytes', r'\1', line).rstrip ()
+			shared = int (re.sub (r',', r'', m))
+
+	return totalLocal + shared, totalLocal, shared
+
+def runAndGetOutput (args, dir):
+	spArgs = shlex.split (args)
+	proc = subprocess.Popen (spArgs, cwd=dir, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+	output = proc.communicate()[0]
+	return output
 
 def run (dir, prog, atMLtons, args):
 	print ("\n-------------------------------\n")
@@ -21,38 +98,29 @@ def run (dir, prog, atMLtons, args):
 		atMLtonString += " " + a
 
 	#run the program and capture the output
-	args = "./" + str(prog) + " @MLton" + atMLtonString + " gc-summary -- " \
-			+ args
-
+	args = "./" + str(prog) + " @MLton" + atMLtonString + " -- " + args
 	log ("\t" + args)
-
-	spArgs = shlex.split (args)
-	proc = subprocess.Popen (spArgs, cwd=dir, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-	output = proc.communicate()[0]
+	runAndGetOutput ("make clean-summaries", dir)
+	output = runAndGetOutput (args, dir)
 
 	#extract statistics from output
 	output = re.sub (r'\n', r' ', str(output))
 	if ("Out of memory" in output):
 		time = "0"
+		(m, ml, ms) = "0", "0", "0"
 	else:
 		time = re.sub (r'.*Time diff:\s*([0-9]*)\s*ms.*', r'\1', output)
+		(m, ml, ms) = getMem (dir)
 	try:
 		int(time)
 	except:
-		time = 0
+		time = "0"
+		(m, ml, ms) = "0", "0", "0"
 	print ("\tCompleted in " + str(time) + " ms")
-	return time
-
-def hsizeToInt (s):
-	intVal = float(s.replace('K','').replace('M','').replace('G',''))
-	if s.endswith('K'):
-		intVal *= 1024
-	elif s.endswith('M'):
-		intVal *= 1024*1024
-	elif s.endswith('G'):
-		intVal *= 1024*1024*1024
-	return intVal
-
+	print ("\t\tTotal Mem: " + bytesIntToString (m, 1))
+	print ("\t\tTotal Local Mem: " + bytesIntToString(ml, 1))
+	print ("\t\tShared Mem: " + bytesIntToString (ms, 1))
+	return (time, m, ml, ms)
 
 def fullParameters():
 	progName = {"BarnesHut": "barnes-hutM-amd64", \
@@ -66,53 +134,29 @@ def fullParameters():
 							"Mergesort": "mergesort-amd64", \
 							"Raytrace": "raytrace-amd64"}
 	args = {"BarnesHut": "", \
-					"BarnesHut2": "4096 64", \
+					"BarnesHut2": "1024 256", \
 					"AllPairs": "512 64", \
 					"Mandelbrot": "", \
 					"KClustering": "0 256 200 50 0", \
 					"TSP": "", \
-					"CountGraphs": "", \
+					"CountGraphs": "1", \
 					"GameOfLife": "64 300", \
 					"Mergesort": "10000", \
 					"Raytrace": "64"}
-	numProcs = [1, 2, 4, 8, 16]
-	maxHeap = {"BarnesHut": ["50M", "40M", "30M", "20M", "10M", "9M", "8M", "7M", \
-													 "6.5M", "6M", "5.5M"], \
-					   "BarnesHut2": ["50M", "40M", "30M", "20M", "10M", "9M", "8M", "7M", \
-													 "6.5M", "6M", "5.5M"], \
-						 "AllPairs": ["50M", "40M", "30M", "20M", "10M", "9M", "8M", "7M", \
-						 							"6.5M", "6M", "5.5M"],
-						 "Mandelbrot": ["50M", "40M", "30M", "25M", "20M", "18M", "16M", \
-								 						"14M", "12M", "11M", "10M", "9M", "8M", "7M", "6M", \
-														"5M", "4M", "3M"], \
-						 "KClustering": ["20M", "15M", "12M", "11M", "10M", "9M", "8M", "7.5M", \
-							 							 "7M", "6.5M", "6M"], \
-					   "TSP": ["200M", "150M", "100M", "96M", "95M", "94.5M"], \
-						 "CountGraphs": ["25M", "15M", "10M", "6M", "4M", "3M", "2.5M", "2M", "1M"], \
-						 "GameOfLife": ["25M", "15M", "10M", "6M", "4M", "3M", "2.5M", "2M", "1M"], \
-						 "Mergesort": ["100M", "50M", "40M", "30M", "25M", "22M", "20M", "19M", "18M"], \
-						 "Raytrace": ["30M", "25M", "22M", "20M", "18M", "16M", "15M"]}
-	return (progName, args, numProcs, maxHeap)
+	numProcs = [16]
+	return (progName, args, numProcs)
 
 def testParameters():
-	progName = {"BarnesHut": "barnes-hutM-amd64", \
-							"AllPairs": "floyd-warshall-amd64", \
-							"Mandelbrot": "mandelbrot-amd64"}
-	args = {"BarnesHut": "", \
-					"AllPairs": "512 64", \
-					"Mandelbrot": ""}
-	numProcs = [1, 2, 4, 8, 16]
-	maxHeap = {"BarnesHut": ["50M"], \
-						 "AllPairs": ["50M", "40M"], \
-						 "Mandelbrot": ["50M", "40M"]}
-	return (progName, args, numProcs, maxHeap)
-
+	progName = {"CountGraphs": "count-graphs-amd64"}
+	args = {"CountGraphs": "1"}
+	numProcs = [16]
+	return (progName, args, numProcs)
 
 def main():
 	#Parse options
 	parser = OptionParser()
 	parser.add_option("-d", "--database", dest="database", help="database location", \
-										metavar="FILE", default="/home/chandras/PLDI/results")
+										metavar="FILE", default="/home/chandras/PLDI/resultsUT")
 	parser.add_option("-b", "--benchmark", dest="bmarkList", action="append", \
 										help="run only the given benchmarks")
 	parser.add_option("-r", "--rerun", dest="rerun", default=False, \
@@ -128,8 +172,7 @@ def main():
 	if (options.bmarkList):
 		benchmarks = options.bmarkList
 	else:
-		benchmarks = ["BarnesHut", "AllPairs", "Mandelbrot", "KClustering", "TSP", \
-									"CountGraphs", "GameOfLife", "Mergesort", "Raytrace", "BarnesHut2"]
+		benchmarks = ["BarnesHut2", "CountGraphs"]
 	(progName, args, numProcs, maxHeap) = fullParameters ()
 
 	nodeKind = ['o-', 's--', 'D-.', 'x:', '^-', 'V--', '>-.', '<:']
@@ -147,31 +190,36 @@ def main():
 		log ("preparing data for plotting heap vs time for " + b)
 
 		#calculate the minimum x
-		c.execute ("select distinct maxHeap from runTime where benchmark=? and result!=0 and gckind='UT'", [b])
-		data = list (map (lambda v: hsizeToInt (v[0]), c.fetchall ()))
+		c.execute ("select distinct maxHeap from runTime where benchmark=? and gckind='UT' and result!=0", [b])
+		data = list (map (lambda v: bytesStringToInt (v[0]), c.fetchall ()))
 		if data:
 			minX = min(data)
 		else:
 			minX = 0
 
-		for n in [1, 2, 4, 8, 16]:
-			c.execute ("select maxHeap, result from runTime where benchmark=? and numProcs=? \
-									and result!=0 and gckind='UT'", (b, n))
-			data = c.fetchall ()
-			x = list (map (lambda v: hsizeToInt (v[0]), data))
-			if x: #x is not empty
-				shouldPlot = True
-				x = [v/minX for v in x]
-				y = list (map (lambda v: v[1], data))
-				plt.plot (x, y, nodeKind[nodeIndex], label="Proc="+str(n))
-				nodeIndex += 1
+	for n in numProcs:
+		c.execute ("select maxHeap, result from runTime where benchmark=? and numProcs=? \
+								and result!=0 and gckind='UT'", (b, n))
+		data = c.fetchall ()
+		x = list (map (lambda v: hsizeToFloat (v[0]), data))
+		if x: #x is not empty
+			shouldPlot = True
+			x = [v/minX for v in x]
+			y = list (map (lambda v: v[1], data))
+			z = list (zip (x,y))
+			z.sort ()
+			x,y = list(zip (*z))
+			l = "P="+str(n)
+			plt.plot (x, y, nodeKind[nodeIndex], label=l)
+			nodeIndex += 1
+			nodeIndex %= len(nodeKind)
 
 		if shouldPlot:
 			log ("plotting heap vs time for " + b)
 			#plot the current graph
 			plt.xlim(xmin = 0)
 			plt.legend ()
-			plt.savefig (b+"_heap_vs_time.eps")
+			plt.savefig (b+"UT_local_heap_vs_time.eps")
 			plt.close ()
 
 
