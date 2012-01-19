123697fced54470b8b1f6668d07e3dd431f1b4d2
kc <chandras@cs.purdue.edu>
2012-01-19 16:18:20 -0500

    Edits to makefiles

--- trunk/testing/lwtgc/benchmarks/CountGraphs/runAzul.sh
+++ trunk/testing/lwtgc/benchmarks/CountGraphs/runAzul.sh
@@ -7,8 +7,8 @@ NUM_PROCS=$3
 echo running $FILENAME
 
 if [ $GDB -eq 1  ]; then
-~/azul/_install/azul/avm-purdue/sandbox/linux/bin/i686/cproxy2 -h 128.10.129.75 -PX:+StartSuspended $FILENAME @MLton number-processors $NUM_PROCS max-heap-local 1K -- 4 8
+~/azul/_install/azul/avm-purdue/sandbox/linux/bin/i686/cproxy2 -h 128.10.129.75 -PX:+StartSuspended $FILENAME @MLton number-processors $NUM_PROCS -- 4 8
 else
 ~/azul/_install/azul/avm-purdue/sandbox/linux/bin/i686/cproxy2 -h 128.10.129.75 -PX:MemCommit=8g \
-	$FILENAME @MLton number-processors $NUM_PROCS gc-summary cumulative -- 8192 9
+	$FILENAME @MLton number-processors $NUM_PROCS gc-summary cumulative -- 4096 8
 fi
